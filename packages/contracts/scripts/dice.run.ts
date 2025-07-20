e36e0ceb22bbc2cd37b6a17fa3dab29a47dd3f64
trietng <93473943+trietng@users.noreply.github.com>
2025-07-20 17:55:26 +0700

    add server drawing

--- packages/contracts/scripts/dice.run.ts
+++ packages/contracts/scripts/dice.run.ts
@@ -5,9 +5,9 @@ import { Dice__factory, Dice, ShuffleManager } from "../types";
 import { deploy_shuffle_manager } from "./dice.deploy";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { v4 as uuidv4 } from 'uuid';
-// import { dnld_aws, P0X_DIR, sleep } from "@zk-shuffle/jssdk/src/shuffle/utility";
-// import { resolve } from "path";
-// import { GameTurn, ZKShuffle } from "@zk-shuffle/jssdk";
+import { P0X_DIR, sleep } from "@zk-shuffle/jssdk/src/shuffle/utility";
+import { resolve } from "path";
+import { GameTurn, ZKShuffle } from "@zk-shuffle/jssdk";
 
 dotenv.config();
 
@@ -66,20 +66,6 @@ class GameService {
 
                 await (await this.shuffleGame.connect(this.players[0]).allowJoinGame(clientId)).wait();
                 return gameId;
-                // if (this.shuffleManager) {
-                    
-                //     // const [playercard0, playercard1] = await Promise.all([this.player_run(this.players[0], gameId), this.player_run(this.players[1], gameId)]);
-                //     console.log("Player 0 revealed card: ", playercard0);
-                //     console.log("Player 1 revealed card: ", playercard1);
-                //     if (playercard0) {
-                //         return playercard0;
-                //     } else {
-                //         return playercard1;
-                //     }
-                // } else {
-                //     throw new Error("ShuffleManager is not initialized");
-                // }
-
             } else {
                 throw new Error("Contracts or players are not initialized");
             }
@@ -87,6 +73,67 @@ class GameService {
             console.error("Error starting new game:", error);
             return null;
         }
+    }
+
+    async playerRun(gameId: number) {
+        const owner = this.players!![0];
+        console.log("Player ", owner.address, "init shuffle context!");
+
+        const player = await ZKShuffle.create(
+            this.shuffleManager!.address,
+            owner,
+            await ZKShuffle.generateShuffleSecret(),
+            resolve(P0X_DIR, "./wasm/decrypt.wasm"),
+            resolve(P0X_DIR, "./zkey/decrypt.zkey"),
+            resolve(P0X_DIR, "./wasm/encrypt.wasm"),
+            resolve(P0X_DIR, "./zkey/encrypt.zkey")
+        );
+        
+        let playerIdx = await player.joinGame(gameId);
+        console.log(
+            "Player ",
+            owner.address,
+            "Join Game ",
+            gameId,
+            " asigned playerId ",
+            playerIdx,
+        );
+        
+        // play game
+        let turn: GameTurn = GameTurn.NOP;
+        let revealedCard: number | null = null;
+        
+        while (turn !== GameTurn.Complete) {
+            turn = await player.checkTurn(gameId);
+
+            if (turn != GameTurn.NOP) {
+            switch (turn) {
+                case GameTurn.Shuffle:
+                console.log("Player ", playerIdx, " 's Shuffle turn!");
+                await player.shuffle(gameId);
+                break;
+                case GameTurn.Deal:
+                console.log("Player ", playerIdx, " 's Deal Decrypt turn!");
+                await player.draw(gameId);
+                break;
+                case GameTurn.Open:
+                console.log("Player ", playerIdx, " 's Open Decrypt turn!");
+                let cards = await player.openOffchain(gameId, [playerIdx]);
+                console.log("Player ", playerIdx, " open offchain hand card ", cards[0]);
+                cards = await player.open(gameId, [playerIdx]);
+                console.log("Player ", playerIdx, " open onchain hand card ", cards[0]);
+                revealedCard = cards[0];
+                break;
+                case GameTurn.Complete:
+                console.log("Player ", playerIdx, " 's Game End!");
+                break;
+                default:
+                console.log("err turn ", turn);
+            }
+            } 
+            await sleep(1000);
+        }  
+        return revealedCard;
     }
 }
 
@@ -139,6 +186,7 @@ async function handleDiceRoll(message: any) {
                     })
                 );
                 console.log(`Sent response to Client ${message.clientId}`);
+                gameService.playerRun(gameId)
             }
         }
 
